d326f9baae4843b89bc78069aee03e3b763d8dce
nicvagn <nicolasvaagen@gmail.com>
2023-10-08 20:05:26 -0600

    removed somethings from app.js

--- website/app.js
+++ website/app.js
@@ -1,13 +1,3 @@
-//lost to time what this does
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-});
-
-
 //configure and display chess board.js
 var chessPosition = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R';
 var correctPosition = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R';
