5cd0f6dddce7816b1cc777f9436d14e1cc5a6aba
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-09-14 13:21:37 -0600

    tournaments: v1.0

--- script/src/tournament_page.ts
+++ script/src/tournament_page.ts
@@ -15,47 +15,115 @@ Players are only eligible to win prizes corresponding to the section they regist
     `Brilliancy Prize: Each section will have a brilliancy prize. Anyone can nominate a ‘brilliancy’ for anyone, but cannot nominate a brilliancy for oneself. The brilliancy must be recorded on both players’ records of the game and should be recreated and presented to the Tournament Director. The brilliancy prize will be considered and chosen on the basis of subjective aesthetic opinion of the TD.`,
   ],
   // 1
-  ["Grand Prix Leg 1 on September 7", "Organized by SCC rapid, idk"],
+  [
+    "Grand Prix Leg 1 on September 7",
+    "Organized by SCC: https://saskatoonchessclub.weebly.com/tournaments.html",
+  ],
   // 2
   [
     "Horizon Classical on October 4-7",
-    "tournament details 2: something about the and rating etc.",
+    `Official CFC Rated Classical Tournament:
+    60 | 30 – 60 minutes with 30 second increment 
+    5-round swiss`,
+    `Sections: Open, U1500, 10 Years Old and Under 
+One ½ point bye available upon request. No final round byes.
+Prizes: Cash Prizes and Medals for 1st, 2nd, and 3rd places.
+1st place Open will have their name carved on to the club trophy.
+Players are only eligible to win prizes corresponding to the section they registered in.
+50% of the registration fee will go to the club, and 50% will go to the cash prizes.`,
+
+    `Brilliancy Prize: Each section will have a brilliancy prize. Anyone can nominate a ‘brilliancy’ for anyone, but cannot nominate a brilliancy for oneself. The brilliancy must be recorded on both players’ records of the game and should be recreated and presented to the Tournament Director. The brilliancy prize will be considered and chosen on the basis of subjective aesthetic opinion of the TD.`,
   ],
   // 3
 
-  ["Grand Prix Leg 2 on November 2", "Organized by SCC, rapid, idk"],
+  [
+    "Grand Prix Leg 2 on November 2",
+    "Organized by SCC: https://saskatoonchessclub.weebly.com/tournaments.html",
+  ],
   // 4
   [
-    "Horizon Classical on December 13-15",
-    "tournament details 3: something about the sections and rating etc.",
+    `Official CFC Rated Classical Tournament:
+    60 | 30 – 60 minutes with 30 second increment 
+    5-round swiss`,
+    `Sections: Open, U1500, 10 Years Old and Under 
+One ½ point bye available upon request. No final round byes.
+Prizes: Cash Prizes and Medals for 1st, 2nd, and 3rd places.
+1st place Open will have their name carved on to the club trophy.
+Players are only eligible to win prizes corresponding to the section they registered in.
+50% of the registration fee will go to the club, and 50% will go to the cash prizes.`,
+
+    `Brilliancy Prize: Each section will have a brilliancy prize. Anyone can nominate a ‘brilliancy’ for anyone, but cannot nominate a brilliancy for oneself. The brilliancy must be recorded on both players’ records of the game and should be recreated and presented to the Tournament Director. The brilliancy prize will be considered and chosen on the basis of subjective aesthetic opinion of the TD.`,
   ],
   // 5
-  ["Grand Prix Leg 3 on January 2", "Organized by SCC, rapid, idk"],
+  [
+    "Grand Prix Leg 3 on January 2",
+    "Organized by SCC: https://saskatoonchessclub.weebly.com/tournaments.html",
+  ],
   // 6
   [
     "Horizon Classical on February 21-23",
-    "tournament details 4: something about the sections and rating etc.",
+    `Official CFC Rated Classical Tournament:
+    60 | 30 – 60 minutes with 30 second increment 
+    5-round swiss`,
+    `Sections: Open, U1500, 10 Years Old and Under 
+One ½ point bye available upon request. No final round byes.
+Prizes: Cash Prizes and Medals for 1st, 2nd, and 3rd places.
+1st place Open will have their name carved on to the club trophy.
+Players are only eligible to win prizes corresponding to the section they registered in.
+50% of the registration fee will go to the club, and 50% will go to the cash prizes.`,
+
+    `Brilliancy Prize: Each section will have a brilliancy prize. Anyone can nominate a ‘brilliancy’ for anyone, but cannot nominate a brilliancy for oneself. The brilliancy must be recorded on both players’ records of the game and should be recreated and presented to the Tournament Director. The brilliancy prize will be considered and chosen on the basis of subjective aesthetic opinion of the TD.`,
   ],
   // 7
-  ["Grand Prix Leg 4 on March 15", "Organized by SCC, rapid, idk"],
+  [
+    "Grand Prix Leg 4 on March 15",
+    "Organized by SCC: https://saskatoonchessclub.weebly.com/tournaments.html",
+  ],
   // 8
   [
     "Horizon Classical on April 11-13",
-    "tournament details 5: something about the sections and rating etc.",
+    `Official CFC Rated Classical Tournament:
+    60 | 30 – 60 minutes with 30 second increment 
+    5-round swiss`,
+    `Sections: Open, U1500, 10 Years Old and Under 
+One ½ point bye available upon request. No final round byes.
+Prizes: Cash Prizes and Medals for 1st, 2nd, and 3rd places.
+1st place Open will have their name carved on to the club trophy.
+Players are only eligible to win prizes corresponding to the section they registered in.
+50% of the registration fee will go to the club, and 50% will go to the cash prizes.`,
+
+    `Brilliancy Prize: Each section will have a brilliancy prize. Anyone can nominate a ‘brilliancy’ for anyone, but cannot nominate a brilliancy for oneself. The brilliancy must be recorded on both players’ records of the game and should be recreated and presented to the Tournament Director. The brilliancy prize will be considered and chosen on the basis of subjective aesthetic opinion of the TD.`,
   ],
   // 9
-  ["Grand Prix Leg 5 on May 10", "Organized by SCC, rapid, idk"],
+  [
+    "Grand Prix Leg 5 on May 10",
+    "Organized by SCC: https://saskatoonchessclub.weebly.com/tournaments.html",
+  ],
   // 10
-  ["Grand Prix Super Final on June 7", "Organized by SCC, rapid, idk"],
+  [
+    "Grand Prix Super Final on June 7",
+    "Organized by SCC: https://saskatoonchessclub.weebly.com/tournaments.html",
+  ],
   // 11
   [
     "Horizon Classical on July 25-27",
-    "tournament details 6: something about the sections and rating etc.",
+    `Official CFC Rated Classical Tournament:
+    60 | 30 – 60 minutes with 30 second increment 
+    5-round swiss`,
+    `Sections: Open, U1500, 10 Years Old and Under 
+One ½ point bye available upon request. No final round byes.
+Prizes: Cash Prizes and Medals for 1st, 2nd, and 3rd places.
+1st place Open will have their name carved on to the club trophy.
+Players are only eligible to win prizes corresponding to the section they registered in.
+50% of the registration fee will go to the club, and 50% will go to the cash prizes.`,
+
+    `Brilliancy Prize: Each section will have a brilliancy prize. Anyone can nominate a ‘brilliancy’ for anyone, but cannot nominate a brilliancy for oneself. The brilliancy must be recorded on both players’ records of the game and should be recreated and presented to the Tournament Director. The brilliancy prize will be considered and chosen on the basis of subjective aesthetic opinion of the TD.`,
   ],
 ];
 
 // get the template strings from html
 const tournamentTemplate: string = $("#show-tournament-template").html();
+
 const joinTemplate: string = $("#join-tournament-template").html();
 
 function showTournamentPg(tournament_num: number) {
@@ -77,9 +145,19 @@ function showTournamentPg(tournament_num: number) {
   $("#tournament-info").show();
 }
 
+function showSCCTournament() {
+  // hide the tournament list and title
+  $("#tournament-list").hide();
+  $("#tournament-title").hide();
+  $("#tournament-info").hide();
+  // render tournament info to the DOM
+  $("#scc-tournament-info").show();
+}
+
 // show the list list view
 function showTournamentLst() {
   $("#tournament-info").hide();
+  $("#scc-tournament-info").hide();
 
   // show the tournament list and title
   $("#tournament-list").show();
